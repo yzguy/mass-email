b6722dbca9afe006aae6afcb37d93740628ff730
Adam Smith <adamsmith@hersheys.com>
2015-09-18 23:33:45 -0400

    add argparse, not working correctly

--- mass-email.py
+++ mass-email.py
@@ -1,11 +1,23 @@
-#!/usr/bin/python
+#!venv/bin/python
 
 # Imports
-import markdown, sys, csv, getpass, smtplib
+import markdown, sys, csv, getpass, smtplib, argparse
 from email.mime.text import MIMEText
 from jinja2 import Template
 
-def login():
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--markdown', help='Path to Markdown Template')
+parser.add_argument('-c', '--csv', help='Path to CSV file')
+args = parser.parse_args() 
+
+markdownf = args.markdown
+csvf = args.csv
+username = raw_input("Username: ")
+password = getpass.getpass("Password: ")
+name = raw_input("Name: ")
+subject = raw_input("Subject: ")
+
+def login(username, password):
     try:
         server = smtplib.SMTP("smtp.gmail.com:587")
         server.starttls()
@@ -16,40 +28,26 @@ def login():
         server.close()
         sys.exit(1)
 
-if (len(sys.argv) == 3):
-    username = raw_input("Username: ") + "@gmail.com"
-    password = getpass.getpass("Password: ")
-    name = raw_input("Name: ")
-    subject = raw_input("Subject: ")
+server = login(username, password, smtp_server, smtp_port)
+emails_sent = 0
 
-    server = login()
-    emails_sent = 0
+with open(markdownf, 'r') as md_file:
+    md_template = Template(md_file.read())
 
-    with open(sys.argv[1], 'r') as md_file:
-        md_template = Template(md_file.read())
+with open(csvf, 'r') as csv_file:
+    csv_data = csv.DictReader(csv_file)
+    for row in csv_data:
+        rendered_template = md_template.render(row)
+        html = markdown.markdown(rendered_template)
 
-    with open(sys.argv[2], 'r') as csv_file:
-        csv_data = csv.DictReader(csv_file)
-        for row in csv_data:
-            rendered_template = md_template.render(row)
-            html = markdown.markdown(rendered_template)
+        msg = MIMEText(html, 'html')
+        msg['Subject'] = subject
+        msg['From'] = name
+        msg['To'] = row['email']
 
-            msg = MIMEText(html, 'html')
-            msg['Subject'] = subject
-            msg['From'] = name
-            msg['To'] = row['email']
+        server.sendmail(username, [row['email']], msg.as_string())
+        print "Email sent to: %s" % row['email']
+        emails_sent += 1
+server.close()
+print "\nTotal Emails Sent:", emails_sent
 
-            server.sendmail(username, [row['email']], msg.as_string())
-            print "Email sent to: %s" % row['email']
-            emails_sent += 1
-    server.close()
-    print "\nTotal Emails Sent:", emails_sent
-else:
-    print """
-Usage: ./markdown-email.py <markdown file> <csv file>
-    <markdown file> - path to markdown file ( email.md )
-    <csv file> - path to csv data file ( data.csv )
-    <verbose> - type verbose to see emails in raw format
-
-    You must enter your email address, password, and subject for email.
-    """
